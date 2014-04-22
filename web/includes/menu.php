e22a29e824e67fffc8c95377b9da2ddec9492f29
Luka Hrgarek <luka.hrgarek@gmail.com>
2014-04-22 18:27:57 +0200

    Rezanje html datoteke na več php-jev

---
+++ web/includes/menu.php
@@ -0,0 +1,34 @@
+		<div class="menu"><!-- start menu -->
+			<ul class="mcd-menu">
+				<li>
+					<a href="index.php">
+						<i class="icon1"></i>
+						<strong>Zacetna</strong>
+					</a>
+				</li>
+				<li>
+					<a href="recepti.php">
+						<i class="icon2"></i>
+						<strong>Recepti</strong>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="icon3"></i>
+						<strong>Nekaj</strong>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="icon4"></i>
+						<strong>Nekaj</strong>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="icon5"></i>
+						<strong>Nekaj</strong>
+					</a>
+				</li>
+			</ul>
+		</div><!-- end menu -->	
