d6c8b2f7dd9fff99cda95f4a4e2c8e07db4b22e2
vpetra <vrbanec.petra@gmail.com>
2014-04-23 00:23:49 +0200

    Login poboljsan
    skoro pa dokoncan

--- web/login.php
+++ web/login.php
@@ -1,6 +1,30 @@
 <?php
 	include("includes/header.php");
 	include("includes/menu.php");
+	include_once ("php/database.php");
+	session_start();
+	$submit=isset($_POST['submit']);
+	
+	
+	if($submit){
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+		$sql="SELECT name, surname, password FROM user WHERE email='$email'";
+		$query= mysqli_query($con, $sql);
+		$rezultat=mysqli_num_rows($query);
+		if ($rezultat>0){
+			while($row=mysqli_fetch_assoc($query)){
+				$dbpassword=$row['password'];
+				if($dbpassword==$password){
+					$_SESSION["login"]=$email;
+					header('Location: index.php');
+				}
+				else{
+					echo "Krivo geslo!";
+				}
+			}
+		}
+	}
 ?>
 	<div class="clear"></div>
 		<div class="spans_of_2"><!-- start spans_of_2 -->
@@ -9,17 +33,15 @@
 					<h4>Prijava</h4>
 				</div>
 				<div class="contact_form">
-					<form method="post" action="php/registration.php" autocomplete="off">
-                   	    <span>Uporabniško ime</span>
-						<input type="text" class="text" name="username" placeholder="pMicka" autocomplete="off" required>
+					<form method="post" autocomplete="off">
+						<span>E-mail</span>
+						<input type="email" class="text" name="email" placeholder="example@example.com" required>
                         <span>Geslo</span>
 						<input type="password" class="text" name="password" placeholder="•••••••" required>
 						<span id="validate"></span>
-						<span>E-mail</span>
-						<input type="email" class="text" name="email" placeholder="mpungartnik@gmail.com" required>
 						<div>
 							<input type="reset" value="Preklici">
-							<input type="submit" value="Prijavi se">
+							<input type="submit" value="Prijavi se" name="submit">
 							<div class="clear"></div>
 						</div>
 					</form>
@@ -27,8 +49,6 @@
 			</div><!-- end span1_of_1 -->
 			<div class="clear"></div>
 		</div><!-- end spans_of_2 -->
-
-
 <?php
 	include("includes/footer.php");
 ?>
