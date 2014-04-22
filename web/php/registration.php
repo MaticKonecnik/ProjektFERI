873827d3f7cfa26ddd4e45d785bb358dd4afc172
Luka Hrgarek <luka.hrgarek@gmail.com>
2014-04-22 17:00:04 +0200

    Dopolnjevanje registracije

--- web/php/registration.php
+++ web/php/registration.php
@@ -7,18 +7,19 @@
 	$email = $_POST['email'];
 	
 	$query = sprintf("INSERT INTO user(username, password, name, surname, email) VALUES('%s', '%s', '%s', '%s', '%s')",
-			mysql_real_escape_string($username),
-			mysql_real_escape_string($password),
-			mysql_real_escape_string($name),
-			mysql_real_escape_string($surname),
-			mysql_real_escape_string($email));
+			mysqli_real_escape_string($con, $username),
+			mysqli_real_escape_string($con, $password),
+			mysqli_real_escape_string($con, $name),
+			mysqli_real_escape_string($con, $surname),
+			mysqli_real_escape_string($con, $email));
+			
 	
 	if(mysqli_query($con, $query)){
 		//preusmeritev na prijavo, ker je zapis v bazo uspešen
 		header("Location: login.php");
 	}
 	else{
-		echo "Registracija neuspešna!";
-		die();
+		
+		die("Registracija neuspešna!".mysqli_error($con));
 	}
 ?>
