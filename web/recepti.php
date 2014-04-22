2ce4f97f2299e2b43c1f28e3731bab9c70989135
Luka Hrgarek <luka.hrgarek@gmail.com>
2014-04-22 23:58:35 +0200

    Malo(!) boljši izpis receptov

--- web/recepti.php
+++ web/recepti.php
@@ -8,27 +8,22 @@
 	$result = mysqli_query($con,$sql);
 
 	while($row = mysqli_fetch_array($result))
-	{
-		echo('<article><h3><a href="recept.php?id=' . $row['id'] . '">' . $row['name'] . '</a></h3>');
-		echo($row['instructions']);	
-		echo('</article>');
-	}
-?>
-		
-			<div class="clear"></div>
-            <div class="grid1_of_list2"><!-- start grid1_of_list2 -->
-				<div class="grid1_of_list">
-					<img src="images/pic2.jpg" alt="" />
-					<h4>naziv recepta</h4>
-				</div>
-				<ul class="list2">
-					<li><p>Kcal</p><span>230</span></li>
-					<li><p>prawns</p> <span>12%</span></li>
+	{ ?>		
+			<div class="span_of_2">
+				<span class="span_of_img"><img src="<?php echo($row['image']); ?>" alt="" class="slika_vsi_recepti"/></span>
+				<div class="span_of_list">
+					<div class="span1_of_1">
+						<h4><?php echo($row['name']); ?></h4>
+					</div>
+					<div class="span1_of_2">
+						<p><?php echo($row['instructions']); ?></p>
+						<a class="arrow" href="#"><span>cake testings & consultations</span></a>
+					</div>	
 					<div class="clear"></div>
-				</ul>
-			</div><!-- end grid1_of_list2 -->
-            			
-
+				</div>	
+			</div>
+            <div class="clear"></div>
 <?php
+	}
 	include("includes/footer.php");
 ?>
