070fe1bac3fc93ab09278362bd48599dab65f1a3
Luka Hrgarek <luka.hrgarek@gmail.com>
2014-04-23 23:09:49 +0200

    CSS login

--- web/login.php
+++ web/login.php
@@ -1,10 +1,9 @@
 <?php
 	include("includes/header.php");
 	include("includes/menu.php");
-	include_once ("php/database.php");
+	include_once ("includes/database.php");
 	session_start();
 	$submit=isset($_POST['submit']);
-	
 	
 	if($submit){
 	$email=$_POST['email'];
@@ -20,7 +19,7 @@
 					header('Location: index.php');
 				}
 				else{
-					echo "Krivo geslo!";
+					echo "Napačno geslo!";
 				}
 			}
 		}
@@ -28,7 +27,7 @@
 ?>
 	<div class="clear"></div>
 		<div class="spans_of_2"><!-- start spans_of_2 -->
-			<div class="span_of_1"><!-- start span_of_1 -->
+			<div class="span_of_1 login_form"><!-- start span_of_1 -->
 				<div class="span_of_text">
 					<h4>Prijava</h4>
 				</div>
