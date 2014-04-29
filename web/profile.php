61576f90d9f0f4d24e832fe8f121fee95937831f
MaticKonecnik <matickcar@gmail.com>
2014-04-29 22:57:21 +0200

    edit profile

--- web/profile.php
+++ web/profile.php
@@ -1,17 +1,65 @@
 <?php
-	session_start();
 	include("includes/header.php");
 	include("includes/menu.php");
 	include_once ("includes/database.php");
 	
 	$id = (int)$_GET['id'];
 	
-	//prijavljen uporabnik, lahko ureja svoj profil
-	if(isset($_SESSION['login']) && $id = $_SESSION['login']){
-		//echo '<a href="editProfile.php">Uredi profil</a>';
-	}
+	$query = "SELECT * FROM user WHERE id = '$id'";
+	$result = mysqli_query($con, $query);
+	$row = mysqli_fetch_array($result);
 ?>
-	
+	<table id="userProfile">
+		<?php	
+			//prijavljen uporabnik, lahko ureja svoj profil
+			if(isset($_SESSION['login']) && $id == $_SESSION['login']){
+				echo '<tr><td colspan="2"><a href="#editProfile">Uredi profil</a></td></tr>';
+			}
+		?>
+		<tr>
+			<td>Ime in priimek:</td>
+			<td><?php echo $row['name']." ".$row['surname']; ?></td>
+		</tr>
+		<tr>
+			<td>E-mail:</td>
+			<td><?php echo $row['email']; ?></td>
+		</tr>
+		<tr>
+			<td>Uporabniško ime:</td>
+			<td><?php echo $row['username']; ?></td>
+		</tr>
+		<tr>
+			<td colspan="2"><img src="<?php echo $row['image'] ?>"</td>
+		</tr>
+	</table>
+	<a href="#_" class="lightbox" id="editProfile">
+		<div class="clear"></div>
+		<div class="spans_of_2"><!-- start spans_of_2 -->
+			<div class="span_of_1"><!-- start span_of_1 -->
+				<div class="span_of_text">
+					<h4>Urejanje profila</h4>
+				</div>
+				<div class="contact_form">
+					<form method="post" autocomplete="off">
+						<span>Ime</span>
+						<input type="text" class="text" name="name" value="<?php echo $row['name']; ?>" />
+						<span>Priimek</span>
+						<input type="text" class="text" name="surname" value="<?php echo $row['surname']; ?>" />
+						<span>Uporabniško ime</span>
+						<input type="text" class="text" name="username" value="<?php echo $row['username']; ?>" />
+						<span>E-mail</span>
+						<input type="email" class="text" name="email" value="<?php echo $row['email']; ?>" />
+						<div class="padd">
+							<input type="reset" value="Preklici">
+							<input type="submit" name="submit" value="Uredi">
+							<div class="clear"></div>
+						</div>
+					</form>
+				</div>			
+			</div><!-- end span1_of_1 -->
+			<div class="clear"></div>
+		</div><!-- end spans_of_2 -->
+	</a>
 <?php
 	include("includes/footer.php");
 ?>
