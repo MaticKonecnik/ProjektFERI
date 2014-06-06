3a93321ae1322d259a145d1ff31746aa196c3116
Luka Hrgarek <luka.hrgarek@gmail.com>
2014-06-07 01:06:51 +0200

    Sestevine na začetni + fonti

--- web/index.php
+++ web/index.php
@@ -32,7 +32,7 @@
 				<div id="ingredient-container"></div>
 			<div class="clear"></div>
 <?php
-	$query = "SELECT DISTINCT(name) FROM ingredient i INNER JOIN has_ingredient hi ON i.id=hi.ingredient_id WHERE LENGTH(name) < 15 LIMIT 30";
+	$query = "SELECT DISTINCT(name) FROM ingredient i INNER JOIN has_ingredient hi ON i.id=hi.ingredient_id WHERE LENGTH(name) < 15 LIMIT 28";
 	$r = mysqli_query($con, $query);
 	$st=0;
 	echo '<form action="" id="forma_za_sestevine">';
@@ -42,7 +42,7 @@
 		echo('<label for="roundedOne_'.$roww[0].'"><span>'.$roww[0].'</span></label>');
 		echo('</div>');
 	}
-	echo '<input type="submit" value="Poišči" name="submit">';
+	echo '<div class="clear"></div><input type="submit" value="Poišči" name="submit">';
 	echo '</form>';
 	include("includes/footer.php");
 ?>
