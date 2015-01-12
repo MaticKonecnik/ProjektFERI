cf0d3b78c56c13574a6ca8d1136621e72a3faa14
Luka Hrgarek <luka.hrgarek@gmail.com>
2015-01-12 15:46:43 +0100

    Krnekaj

--- web/js/grafika/grafika.js
+++ web/js/grafika/grafika.js
@@ -1,8 +1,12 @@
 var scene, renderer, user;
 
 $(document).ready(function() {
-	$.getScript('js/grafika/grafika_init.js', function() {
-		$.getScript('js/grafika/grafika_models.js', function() {});
-		$.getScript('js/grafika/grafika_character.js', function() {}); //character
-	});
+$.getScript('js/grafika/grafika_init.js', function() {
+
+$.getScript('js/grafika/grafika_models.js', function() {});
+
+$.getScript('js/grafika/grafika_character.js', function() {}); //character
+
+
+});
 });
