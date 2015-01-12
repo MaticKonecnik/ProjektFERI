dbb888eada9727d59f9065e23bd923a2ae7cba33
Luka Hrgarek <luka.hrgarek@gmail.com>
2015-01-12 18:44:43 +0100

    2D tekst

--- web/js/grafika/grafika.js
+++ web/js/grafika/grafika.js
@@ -3,11 +3,8 @@ var obstacles = [];
 
 $(document).ready(function() {
 $.getScript('js/grafika/grafika_init.js', function() {
-
-$.getScript('js/grafika/grafika_models.js', function() {});
-
-$.getScript('js/grafika/grafika_character.js', function() {}); //character
-
-
+	$.getScript('js/grafika/grafika_models.js', function() {});
+	$.getScript('js/grafika/grafika_character.js', function() {});
+	$.getScript('js/grafika/grafika_2D_text.js', function() {});
 });
 });
