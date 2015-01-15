68fa187923c29f6a494b16f68684a856099f6790
matijaperhoc <perhocmatija@gmail.com>
2015-01-15 16:32:49 +0100

    EventListener

--- web/js/grafika/grafika.js
+++ web/js/grafika/grafika.js
@@ -8,5 +8,6 @@ $.getScript('js/grafika/grafika_init.js', function() {
 	$.getScript('js/grafika/grafika_character.js', function() {});
 	$.getScript('js/grafika/grafika_2D_text.js', function() {});
 	$.getScript('js/grafika/grafika_video.js', function() {});
+	$.getScript('js/grafika/grafika_plocica.js', function() {});
 });
 });
